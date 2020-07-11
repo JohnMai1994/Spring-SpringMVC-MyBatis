300599871468339ecd6451ed3b41a3d92710265a
John Mai <44597440+JohnMai1994@users.noreply.github.com>
2020-07-11 15:52:28 -0400

    Spring day03

---
+++ Spring/project/spring_day03account/src/main/java/com/jiadong/service/impl/AccountServiceImpl.java
@@ -0,0 +1,66 @@
+package com.jiadong.service.impl;
+
+import com.jiadong.dao.IAccountDao;
+import com.jiadong.domain.Account;
+import com.jiadong.service.IAccountService;
+
+import java.util.List;
+
+public class AccountServiceImpl implements IAccountService {
+
+    private IAccountDao accountDao;
+
+    public IAccountDao getAccountDao() {
+        return accountDao;
+    }
+
+    public void setAccountDao(IAccountDao accountDao) {
+
+        this.accountDao = accountDao;
+    }
+
+
+    @Override
+    public List<Account> findAllAccount() {
+
+        return accountDao.findAllAccount();
+    }
+    @Override
+    public Account findAccountById(Integer accountId) {
+
+        return accountDao.findAccountById(accountId);
+    }
+    @Override
+    public void saveAccount(Account account) {
+        accountDao.saveAccount(account);
+
+    }
+    @Override
+    public void updateAccount(Account account) {
+        accountDao.updateAccount(account);
+    }
+
+    @Override
+    public void deleteAccount(Integer accountId) {
+        accountDao.deleteAccount(accountId);
+    }
+
+
+    @Override
+    public void transfer(String sourceName, String targetName, Float money) {
+        // 1. 根据名称查询转出用户
+        Account source = accountDao.findAccountByName(sourceName);
+        // 2. 根据名称查询转入用户
+        Account target = accountDao.findAccountByName(targetName);
+        // 3. 转出账户减钱
+        source.setMoney(target.getMoney() - money);
+        // 4. 转入用户加钱
+        target.setMoney(target.getMoney() + money);
+        // 5. 更新转出账户
+        accountDao.updateAccount(source);
+        // 6. 更新转入账户
+        accountDao.updateAccount(target);
+
+
+    }
+}
